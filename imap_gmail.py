ec6c92be5078f6f1630f6fb373ec692f17b1aecc
anuraggoyal <55345266+anuragnitt@users.noreply.github.com>
2020-08-09 19:14:34 +0530

    resolved issues regarding download stall

--- imap_gmail.py
+++ imap_gmail.py
@@ -4,13 +4,11 @@ import os
 import re
 import dateutil.parser
 import sys
-import webbrowser
+
+#######################################################################################################################################
 
 enable_imap_url = 'https://mail.google.com/mail/u/0/#settings/fwdandpop'
 app_access_url = 'https://myaccount.google.com/lesssecureapps'
-
-webbrowser.open(app_access_url, new=1)
-webbrowser.open(enable_imap_url, new=1)
 
 print('\t\tDOWNLOADING NECESSARY MODULES\n\n')
 
@@ -20,6 +18,8 @@ os.system('pip3 install secure-imaplib')
 print('\n\t\tDOWNLOAD COMPLETE\n\n')
 print('*'*70 + '\n\n')
 
+#######################################################################################################################################
+
 def check_isdir(dirname) :
 
 	if os.path.isdir(dirname) :
@@ -27,6 +27,8 @@ def check_isdir(dirname) :
 
 	else :
 		os.mkdir(dirname)
+
+#######################################################################################################################################
 
 def export_inbox(host, port, username, password, fetch_protocol) :
 
@@ -55,12 +57,11 @@ def export_inbox(host, port, username, password, fetch_protocol) :
 
 	conn.select('INBOX')
 
-	inbox_bytes = conn.uid('search', None, 'ALL')[1][0].split()
+	inbox_bytes = conn.uid('search', None, 'ALL')[1][0].split()[::-1]
 
-	email_counter = len(inbox_bytes)
 	display_value = 1
 
-	print(f'You have {email_counter} mails in your inbox.\n\n')
+	print(f'You have {len(inbox_bytes)} mails in your inbox.\n\n')
 
 	for fetch_uid in inbox_bytes :
 
@@ -70,57 +71,63 @@ def export_inbox(host, port, username, password, fetch_protocol) :
 
 		email_data = email.message_from_bytes(email_data[0][1])
 
-		email_info = []
+		email_info = {'Subject' : '', 'From' : '', 'To' : '', 'Date' : ''}
+		disposition = []
 
 		for email_part in email_data.walk() :
 
-			if bool(email_part['Subject']) :
-				email_info.append(email_part['Subject'])
+			if type(email_part['Subject']) == type('') : # email without subject will have an empty string as subject.
+
+				if len(email_part['Subject']) > 0 :
+					email_info['Subject'] = email_part['Subject']
 
 			if bool(email_part['From']) :
-				email_info.append(email_part['From'])
+				email_info['From'] = email_part['From']
 
 			if bool(email_part['To']) :
-				email_info.append(email_part['To'])
+				email_info['To'] = email_part['To']
 
 			if bool(email_part['Date']) :
-				email_info.append(email_part['Date'])
+				email_info['Date'] = email_part['Date']
 
-			if bool(email_part.get_filename()) :
-				email_info.append(email_part)
+			if bool(email_part['Content-Disposition']) :
+				disposition.append(email_part)
 
-		email_subject = email_info[0]
+		email_subject = email_info['Subject']
+		if not bool(email_subject) :
+			email_subject = '(no subject)'
 
-		email_sender = email_info[1]
+		email_sender = email_info['From']
 		email_sender = re.findall('[^<> ]+@[^<> ]+', email_sender)[0]
 
-		email_receiver = email_info[2]
+		email_receiver = email_info['To']
+		if not bool(email_receiver) :
+			email_receiver = '(empty)'
 
-		email_time = email_info[3]
+		email_time = email_info['Date']
 		sub_dirname = dateutil.parser.parse(email_time).astimezone(dateutil.tz.tzlocal()).strftime('%d-%m-%Y_%H-%M-%S')
-		sub_dirname = f'{email_counter}_{email_sender}_{sub_dirname}'
+		sub_dirname = f'{display_value}_{email_sender}_{sub_dirname}'
 		datetime = dateutil.parser.parse(email_time).astimezone(dateutil.tz.tzlocal()).strftime('%d/%m/%Y %X')
 
 		sub_dirname = os.path.join(os.getcwd(), sub_dirname)
 		check_isdir(sub_dirname)
 		os.chdir(sub_dirname)
 
-		attachment_count = len(email_info) - 4
+		attachment_dir = os.path.join(os.getcwd(), 'atttachments')
 
-		if attachment_count :
+		for part in disposition :
 
-			attachment_dir = os.path.join(os.getcwd(), 'attachments')
-			check_isdir(attachment_dir)
-			os.chdir(attachment_dir)
+			filename = part.get_filename()
 
-		for i in range(attachment_count) :
+			if filename : # 'inline disposition' may or may not have an attachment
 
-			that_part = email_info[4 + i]
+				filename = os.path.basename(filename)
 
-			filename = os.path.basename(that_part.get_filename())
+				check_isdir(attachment_dir)
+				os.chdir(attachment_dir)
 
-			with open(filename, 'wb') as file :
-				file.write(that_part.get_payload(decode=True))
+				with open(filename, 'wb') as file :
+					file.write(part.get_payload(decode=True))
 
 		os.chdir(sub_dirname)
 
@@ -143,7 +150,6 @@ def export_inbox(host, port, username, password, fetch_protocol) :
 
 		os.chdir(download_dir)
 
-		email_counter -= 1
 		display_value += 1
 
 	print('Logging out .....')
@@ -154,11 +160,10 @@ def export_inbox(host, port, username, password, fetch_protocol) :
 
 	print(f'All mails have been saved to {download_dir}')
 
+#######################################################################################################################################
+
 IMAP_HOST = 'imap.gmail.com'
 IMAP_SSL_PORT = 993
-
-#username = os.environ.get('agmail_addr')
-#password = os.environ.get('agmail_pass')
 
 print('Enter your Gmail login credentials.\nDon\'t worry, they will be transferred over an SSL encrypted network.\n')
 
